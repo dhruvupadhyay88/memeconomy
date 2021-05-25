5a53696b6607260643fc997da99d4c1908be7ba8
Dhruv <dhruvupadhyay88@gmail.com>
2021-05-24 20:36:43 -0400

    praw initial setup

--- api/app.py
+++ api/app.py
@@ -1,26 +1,12 @@
 from flask import Flask
+from flask import request, render_template, jsonify, url_for
 from flask_sqlalchemy import SQLAlchemy
-#from api import create_app, db
-
-#app = create_app()
-#with app.app_context():
-#        db.create_all()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:dhruv@localhost/memeconomy'
 app.debug = True
 
 db = SQLAlchemy(app)
-
-class TodoModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(240), nullable=False)
-    likes = db.Column(db.Integer, default=0)
-
-    def __init__(self, id, content, likes):
-        self.id = id
-        self.content = content
-        self.likes = likes
 
 @app.route('/api/yo', methods=['GET'])
 def index():
