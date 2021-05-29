84b73b5126c8b3746532d3c70a9a03c076c17c4f
Dhruv <dhruvupadhyay88@gmail.com>
2021-05-29 01:26:26 -0400

    routes return values, do python app.py

--- api/app.py
+++ api/app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from logging import WARN
+from flask import Flask, json
 from flask import request, render_template, jsonify, url_for
 from flask_sqlalchemy import SQLAlchemy
 from index import *
 from models import *
 
-
-@app.route('/api/yo', methods=['GET'])
-def index():
+@app.route('/', methods=['GET'])
+def hello_world():
     return {
         'name': 'Hello World'
     }
+
+@app.route('/api/top/daily', methods=['GET'])
+def top_daily():
+    stocks = WallStreetBets.query.order_by(WallStreetBets.mentions.desc()).limit(10).all()
+    res = []
+    for stock in stocks:
+        res.append(stock.mentions)
+    return {"res": res}
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
--- api/index.py
+++ api/index.py
@@ -3,9 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:dhruv@localhost/memeconomy'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.debug = True
 
 db = SQLAlchemy(app)
 
-if __name__ == '__main__':
-    app.run(debug=True)
